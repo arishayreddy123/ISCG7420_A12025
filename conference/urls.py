f0f2730e3556049cd92fac9882896d6bc58a9305
Arishay <arishay.r16@gmail.com>
2025-05-07 20:14:59 +1200

    Include reservations URLs in project

--- conference/urls.py
+++ conference/urls.py
@@ -3,13 +3,10 @@ from django.urls import path, include
 from reservations import views as res_views
 
 urlpatterns = [
-    # Public pages
     path('home/',     res_views.home,     name='home'),
     path('register/', res_views.register, name='register'),
-    # Auth (login/logout)
     path('accounts/', include('django.contrib.auth.urls')),
-    # Reservations app
+    # include the reservations app URLs under the default path
     path('', include('reservations.urls', namespace='reservations')),
-    # Admin site
     path('admin/', admin.site.urls),
 ]
