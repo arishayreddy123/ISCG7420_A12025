cd115adc37f9d6c8eafded56782e78d22918b8ad
Arishay <arishay.r16@gmail.com>
2025-05-07 19:43:30 +1200

    Update URLs and base.html with full navigation and auth

--- conference/urls.py
+++ conference/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 from reservations import views as res_views
 
 urlpatterns = [
+    # Public pages
     path('home/',     res_views.home,     name='home'),
     path('register/', res_views.register, name='register'),
+    # Auth (login/logout)
     path('accounts/', include('django.contrib.auth.urls')),
-    path('',          include('reservations.urls', namespace='reservations')),
-    path('admin/',    admin.site.urls),
+    # Reservations app
+    path('', include('reservations.urls', namespace='reservations')),
+    # Admin site
+    path('admin/', admin.site.urls),
 ]
--- reservations/urls.py
+++ reservations/urls.py
@@ -4,9 +4,13 @@ from . import views
 app_name = "reservations"
 
 urlpatterns = [
+    # List rooms & availability
     path('', views.room_status, name='room_status'),
+    # Make a new reservation
     path('reserve/<int:room_id>/', views.make_reservation, name='make_reservation'),
+    # See your bookings
     path('my-reservations/', views.my_reservations, name='my_reservations'),
+    # Edit or cancel an existing booking
     path('edit/<int:res_id>/', views.edit_reservation, name='edit_reservation'),
     path('cancel/<int:res_id>/', views.cancel_reservation, name='cancel_reservation'),
 ]
