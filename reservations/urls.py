d7b6921ace4e0de7ae152f9cfdadcfd4b6c02205
Arishay <arishay.r16@gmail.com>
2025-05-07 20:19:36 +1200

    Add confirm_reservation URL

--- reservations/urls.py
+++ reservations/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     path('', views.room_status, name='room_status'),
     # confirmation step before booking
     path('confirm/<int:room_id>/', views.confirm_reservation, name='confirm_reservation'),
-    # actual booking action
+    # final booking action
     path('reserve/<int:room_id>/', views.make_reservation, name='make_reservation'),
-    # list, edit, cancel
+    # list my bookings
     path('my-reservations/', views.my_reservations, name='my_reservations'),
+    # edit & cancel
     path('edit/<int:res_id>/', views.edit_reservation, name='edit_reservation'),
     path('cancel/<int:res_id>/', views.cancel_reservation, name='cancel_reservation'),
 ]
